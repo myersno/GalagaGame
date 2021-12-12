dc056fc68389af3d4541b961d35a6ad73f5c45d3
Noah Myers <myersno@mail.gvsu.edu>
2021-12-12 14:44:51 -0500

    what the fuck

--- galaga_student/enemy.py
+++ galaga_student/enemy.py
@@ -2,3 +2,27 @@ import os
 import pygame as pg
 
 # Complete me! - TODO
+
+class Enemy(pg.sprite.Sprite):
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.image.load("enemy.png").convert()
+        self.image.set_colorkey(white)
+        self.rect = self.image.get_rect()
+        self.rect.x = 0
+        self.rect.y = random.randrange(0, 200)
+        self.speedx = random.randrange(1,2)
+
+    def update(self):
+        self.rect.x += self.speedx
+        if self.rect.right > screenWidth:
+            self.rect.x = 50
+            self.rect.y = random.randrange(0, 250)
+            self.speedx = random.randrange(1,3)
+        if self.rect.x > screenWidth:
+            self.kill()
+
+    def shootEnemy(self):
+         bulletEnemy = BulletEnemy(self.rect.centerx, self.rect.bottom)
+         allSprites.add(bulletEnemy)
+         bulletsEnemy.add(bulletEnemy)
