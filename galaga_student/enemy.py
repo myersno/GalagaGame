c5aff276ab040f63d363e08c57b2aefab157a55e
Noah Myers <myersno@mail.gvsu.edu>
2021-12-12 20:10:26 -0500

    Changes, mostly comments for Enemy class

--- galaga_student/enemy.py
+++ galaga_student/enemy.py
@@ -4,6 +4,7 @@ import pygame as pg
 # Complete me! - TODO
 
 class Enemy(pg.sprite.Sprite):
+    # Load the sprite with an image
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
         self.image = pg.image.load("enemy.png").convert()
@@ -12,7 +13,7 @@ class Enemy(pg.sprite.Sprite):
         self.rect.x = 0
         self.rect.y = random.randrange(0, 200)
         self.speedx = random.randrange(1,2)
-
+    # Make the enemies a random distance from the player (speed is a bit weird)
     def update(self):
         self.rect.x += self.speedx
         if self.rect.right > screenWidth:
@@ -21,7 +22,7 @@ class Enemy(pg.sprite.Sprite):
             self.speedx = random.randrange(1,3)
         if self.rect.x > screenWidth:
             self.kill()
-
+    # Enemies go pew pew
     def shootEnemy(self):
          bulletEnemy = BulletEnemy(self.rect.centerx, self.rect.bottom)
          allSprites.add(bulletEnemy)
